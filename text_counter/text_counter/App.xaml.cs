5ef5290bbd2150b744330ef54ae4229abb486516
mizugame634978 <ryouta1853@gmail.com>
2026-05-24 15:58:00 +0900

    init

---
+++ text_counter/text_counter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace text_counter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
